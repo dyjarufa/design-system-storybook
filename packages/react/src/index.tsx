215472a54708f3d8bf5b6cba823248b210da28f6
Jady Rufino <jadyrufa@gmail.com>
2022-11-30 09:56:17 +0000

    feat: implement storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,8 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$gray500',
   paddingcd: '$4',
   borderRadius: '$md',
 })
-
-export function App() {
-  return <h1>Hello world</h1>
-}
